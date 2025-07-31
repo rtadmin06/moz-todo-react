56a4310d42839c192f41bdca666895a173a85fad
rt <2970013550@qq.com>
2025-07-31 22:37:21 +0800

    return first

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 function Form(props) {
   function handleSubmit(event) {
   event.preventDefault();
-  props.onSubmit("Say helloo!");
+  props.onSubmit("Say hellogit!");
 }
 
 
