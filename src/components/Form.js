bf4e761f76841b107b07d64406f3afe6c0dda23e
rt <2970013550@qq.com>
2025-07-31 22:24:41 +0800

    update helloo

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 function Form(props) {
   function handleSubmit(event) {
   event.preventDefault();
-  props.onSubmit("Say hello!");
+  props.onSubmit("Say helloo!");
 }
 
 
